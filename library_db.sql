30ba941d83d7ca9ed3c18507e0049e1333a3a380
angularOcean <liher@oregonstate.edu>
2022-04-25 19:12:12 -0700

    added code\

--- library_db.sql
+++ library_db.sql
@@ -1,6 +1,59 @@
 /*
-CS 340 Project
-Group 2
+CS 340 Project: Spring 2022
+Group 2: Jenna Bucien & Herakles Li
 Penguin Library System
 */
 
+/*This temporarily disables referential constraints 
+when needing to re-create or reload many tables */
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS Publishers;
+DROP TABLE IF EXISTS Authors;
+DROP TABLE IF EXISTS BookCopies;
+DROP TABLE IF EXISTS Patrons;
+DROP TABLE IF EXISTS Checkouts;
+DROP TABLE IF EXISTS CheckedBooks;
+DROP TABLE IF EXISTS Locations;
+
+/*
+Books
+*/
+
+
+/*
+Authors - H
+*/
+
+
+
+/*
+BookCopies 
+*/
+
+
+/*
+Patrons - H
+*/
+
+
+/*
+Checkouts
+*/
+
+
+/*
+CheckedBooks - H
+*/
+
+
+
+/*
+Publishers - H
+*/
+
+
+/*
+Locations
+*/
