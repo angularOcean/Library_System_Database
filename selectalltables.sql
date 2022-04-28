f02f81a4e2cb2121763b0e826beb494bc960e57b
angularOcean <liher@oregonstate.edu>
2022-04-27 18:39:08 -0700

    updated sql code

---
+++ selectalltables.sql
@@ -0,0 +1,9 @@
+show tables;
+select * from authors;
+select * from books;
+select * from bookcopies;
+select * from checkouts;
+select * from locations;
+select * from checkedbooks;
+select * from patrons;
+select * from publishers;
