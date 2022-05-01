dacefee639c064bd2db725ca34da6e9f964eadc1
angularOcean <liher@oregonstate.edu>
2022-05-01 16:50:54 -0700

    renamed selectalltables

--- library_sample_data_manipulation_queries.sql
+++ library_sample_data_manipulation_queries.sql
@@ -1,3 +1,12 @@
+/*
+ CS 340 Project: Spring 2022
+ Group 2: Jenna Bucien & Herakles Li
+ Penguin Library System
+
+ Sample data manipulation queries
+ */
+
+
 show tables;
 select * from authors;
 select * from books;
