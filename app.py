64f6eea94e208b2f7e40db8d64e9f25e420a79fc
angularOcean <liher@oregonstate.edu>
2022-05-01 18:18:35 -0700

    updated app.py

--- app.py
+++ app.py
@@ -7,9 +7,20 @@ app = Flask(__name__)
 
 # Routes 
 
+#1. index.html
 @app.route('/index.html')
 def root():
     return render_template("main.j2")
+
+
+#2. authors.html
+#3. books.html
+#4. bookcopies.html
+#5. patrons.html
+#6. checkouts.html
+#7. checkedbooks.html
+#8. publishers.html
+#9. locations.html
 
 # Listener
 
