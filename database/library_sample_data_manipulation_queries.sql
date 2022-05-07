3d69be398cc5585f90bfc5e9b6c2b5f983a0278b
jbucien <jennabucien@gmail.com>
2022-05-07 11:31:00 -0700

    fixed vscode autoformatter to allow blank lines

--- database/library_sample_data_manipulation_queries.sql
+++ database/library_sample_data_manipulation_queries.sql
@@ -33,12 +33,14 @@ select author_first,
   author_last
 from Authors
 order by author_last asc;
+
 /* Insert */
 insert into (Authors.first_name, Authors.last_name)
 values (
     :author_first_name_input,
     :author_last_name_input
   );
+
 /* Update */
 update Authors
 set author_first = :author_first_name_input,
@@ -49,12 +51,14 @@ where author_id = (
     where author_first = :author_first_selected
       and author_last = :author_last_selected
   );
+
 /* Delete */
 delete from Authors
 where author_first = :author_first_name_input
   and author_last = :author_last_name_input;
 /*
  
+
  /* ----------- BOOKS QUERIES---------------
  CREATE/INSERT
  READ/SELECT
@@ -89,7 +93,6 @@ insert into Books(isbn, title, year, author_id, publisher_id)
 values(:isbn_input, :title_input, :year_input, :author_id, :publisher_id);
 
 
-
 /* Delete */
 delete from Books 
 where book_isbn = :book_isbn_input;
@@ -101,6 +104,7 @@ where book_isbn = :book_isbn_input;
  DELETE
  pages: bookcopies.html
  */
+
 /* Generate Initial Table View */
 select Books.title,
   Authors.author_first,
@@ -111,6 +115,7 @@ from Locations
   inner join Books on BookCopies.book_id = Books.book_id
   inner join Authors on Books.author_id = Authors.author_id
 order by Books.title asc;
+
 /* Generate List of Books Currently On Shelf */
 select Books.title,
   Authors.author_first,
@@ -124,6 +129,7 @@ from Locations
 where CheckedBooks.returned is null
   or CheckedBooks.returned = 1
 order by Books.title asc;
+
 /* Generate List of Books Currently Checked Out 
  with Return Dates
  */
@@ -138,6 +144,7 @@ from Locations
   inner join Checkouts on CheckedBooks.checkout_id = Checkouts.checkout_id
 where CheckedBooks.returned = 0
 order by Books.title asc;
+
 /* Create New Book Copy By Searching for Book ISBN. If ISBN is not found, redirect person to Create New Book Table */
 select book_id
 from Books
@@ -149,9 +156,12 @@ where location.location_name = :location_name_input;
 /* insert into BookCopies */
 insert into BookCopies (book_id, location_id)
 values (:book_id, :location_id);
+
 /* Delete Book Copy */
 delete from BookCopies
 where book_copy_id = :book_copy_selected_from_table;
+
+
 /* ----------- PATRONS QUERIES---------------
  CREATE/INSERT
  READ/SELECT
@@ -197,6 +207,7 @@ delete from Patrons
 	and patron_last = :patron_last_input,
     and email = :email_input;
 
+
 /* ----------- CHECKOUTS QUERIES---------------
  CREATE/INSERT
  READ/SELECT
@@ -205,6 +216,7 @@ delete from Patrons
  
  pages: checkouts.html
  */
+
 /* SQL to Generate Intitial Checkouts Table View */
 select Patrons.patron_first,
   Patrons.patron_last,
@@ -213,6 +225,7 @@ select Patrons.patron_first,
 from Patrons
   inner join Checkouts on Patrons.patron_id = Checkouts.patron_id
 order by Checkouts.checkout_date desc;
+
 /* Checkouts INSERT */
 /* get patron_id from patron's name */
 select patron_id
@@ -226,8 +239,9 @@ values (
     :return_date_input,
     :patron_id_found_from_select_query
   )
-  /* Checkouts UPDATE - allow for change of checkout date, return date, patron. Books checked out changed through CheckedBooks. */
-  /* get patron_id from patron's name */
+
+/* Checkouts UPDATE - allow for change of checkout date, return date, patron. Books checked out changed through CheckedBooks. */
+/* get patron_id from patron's name */
 select patron_id
 from Patrons
 where patron_first = :patron_first_input
@@ -238,6 +252,7 @@ set patron_id = :patron_id_found_from_select_query,
   checkout_date = :checkout_date_input,
   return_date = :return_date_input
 where checkout_id = :checkout_id_selected;
+
 /* Checkouts DELETE - deletes entire Checkout */
 delete from Checkouts
 where checkout_id = :checkout_id_selected;
@@ -287,23 +302,28 @@ where CheckedBooks_id = :checkedbooks_id_input;
 
 
   /* I was thinking that maybe we should have /checkedbooks.html redirect from clicking on a checkout_id from the Checkouts table. Then it still counts as a separate page. So, a user would first add a Checkout. Then, the checkout would appear on the table. Then, the user would click on the Checkout id in the table and add CheckedBooks from there.*/
-  /* ----------- PUBLISHERS QUERIES---------------
-   /*
-   Publishers queries
-   CREATE/INSERT
-   READ/SELECT
-   UPDATE
-   DELETE
-   
-   pages: publishers.html
-   */
-  /* Generate Intitial Table View */
+
+
+/* ----------- PUBLISHERS QUERIES---------------
+  /*
+  Publishers queries
+  CREATE/INSERT
+  READ/SELECT
+  UPDATE
+  DELETE
+  
+  pages: publishers.html
+  */
+
+/* Generate Intitial Table View */
 select Publishers.publisher_name
 from Publishers
 order by Publishers.publisher_name asc;
+
 /* Insert Publishers */
 insert into Publishers (publisher_name)
 values (:publisher_name_input);
+
 /* Update Publishers */
 update Publishers
 set publisher_name = :publisher_name_input
@@ -312,6 +332,7 @@ where author_id = (
     from Publishers
     where publisher_name = :publisher_name_selected
   );
+  
 /* Delete Publishers */
 delete from Publishers
 where publisher_name = :publisher_name_selected;
