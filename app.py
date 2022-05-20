f9786a5917c4f828bf3d833ac4dbfbe076fd21b7
jbucien <jennabucien@gmail.com>
2022-05-20 12:18:31 -0700

    finished publishers crud

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ else:
     )
 
 
-# Routes
+# -----------HOME PAGE-----------
 @app.route("/")
 def index():
     return render_template("home_template.j2")
@@ -501,32 +501,85 @@ def checkedbooks_page():
 
 # -----------PUBLISHERS-----------
 # 8. publishers.html
-@app.route("/publishers.html")
+@app.route("/publishers.html", methods=["POST", "GET"])
 def publishers_page():
     query = """ 
-    select Publishers.publisher_name
+    select publisher_id,
+    publisher_name
     from Publishers
     order by Publishers.publisher_name asc;
     """
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()
-    publishers_headings = ["Publisher Name"]
-    return render_template(
-        "table_template.j2",
-        title="Book Publishers",
-        description="This is a database of publishers",
-        headings=publishers_headings,
-        data=results,
-    )
-
+    publishers_headings = ["ID", "Publisher Name"]
 
 # publishers INSERT
+    if request.method == "POST":
+        request.form.get("insert Publishers")
+        publisher_name = request.form["Publisher Name"]
+        query = (
+            f"INSERT INTO Publishers(publisher_name) VALUES (%s); "
+        )
+        cursor = db.execute_query(
+            db_connection=db_connection,
+            query=query,
+            query_params=(publisher_name,),
+        )
+        return redirect("/publishers.html")
 
+    return render_template(
+    "table_template.j2",
+    title="Book Publishers",
+    description="This is a database of publishers",
+    headings=publishers_headings,
+    data=results,
+    routeURL="publisher",
+)
 
 # publishers UPDATE
+@app.route("/update_publisher/<int:id>", methods=["POST", "GET"])
+def publishers_edit(id):
+    if request.method == "GET":
+        query = "SELECT publisher_name FROM Publishers WHERE publisher_id = %s"
+        curr = db.execute_query(
+            db_connection=db_connection, query=query, query_params=(id,)
+        )
+        info = curr.fetchall()
+        print(info)
 
+    publishers_edit_headings = ["Publisher Name"]
+
+    if request.method == "POST":
+        request.form.get("Update Publisher")
+        publisher_name = request.form["Publisher Name"]
+        publisher_id = id
+        query = f"update Publishers set publisher_name = %s where publisher_id = %s;"
+        curr = db.execute_query(
+            db_connection=db_connection,
+            query=query,
+            query_params=(publisher_name, publisher_id),
+        )
+        return redirect("/publishers.html")
+
+    return render_template(
+        "update_template.j2",
+        data=info,
+        description="Editing Publisher: ",
+        headings= publishers_edit_headings,
+        title="Publisher",
+        routeURL="publishers",
+    )
 
 # publishers DELETE
+@app.route("/delete_publisher/<int:id>", methods=["GET", "POST"])
+def delete_publisher(id):
+    query = "DELETE FROM Publishers WHERE publisher_id = %s"
+    curr = db.execute_query(
+        db_connection=db_connection, query=query, query_params=(id,)
+    )
+    return redirect("/publishers.html")
+
+
 
 # -----------LOCATIONS-----------
 # 9. locations.html
@@ -603,7 +656,6 @@ def locations_edit(id):
         title="Location",
         routeURL="locations",
     )
-
 
 # locations DELETE
 @app.route("/delete_location/<int:id>", methods=["GET", "POST"])
